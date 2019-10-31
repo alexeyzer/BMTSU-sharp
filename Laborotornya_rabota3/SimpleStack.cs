5a78c5c30b7c65942a0f9d5539ab69beb9f9d4e9
Алексей Зудин <alexeyz.zudin@gmail.com>
2019-10-31 20:35:48 +0300

    add

---
+++ Laborotornya_rabota3/SimpleStack.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Laborotornya_rabota3
+{
+    class SimpleStack<T>: SimpleList<T>: where T : IComparable
+    { 
+      public void Push(T element) 
+        {  
+            Add(element); 
+        }
+  
+       
+     
+    }
