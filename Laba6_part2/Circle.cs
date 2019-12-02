c1cff1675fbf0ae2d3d9d1aa1952415636472917
алексей зудин <appstore909@yandex.ru>
2019-12-02 09:43:55 +0300

    add laba6

---
+++ Laba6_part2/Circle.cs
@@ -0,0 +1,46 @@
+﻿using Labaratornay_rabota2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba6_part2
+{
+    public class Circle : geometr_figur
+    {
+        private double radius;
+        public double property_radius
+        {
+            get
+            {
+                return radius;
+            }
+            set
+            {
+                radius = value;
+            }
+        }
+        public Circle(double radius)
+        {
+            this.radius = radius;
+            this.Type = "Круг";
+        }
+        public override double ploshad()
+        {
+            return (Math.PI * radius * radius);
+        }
+        public Circle()
+        {
+            this.Type = "Круг";
+        }
+        public int example(int x, int y)
+        {
+            return (x + y);
+        }
+        public void Print()
+        {
+            Console.WriteLine(this.ToString());
+        }
+    }
+}
---
+++ Laba6_part2/NewAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba6_part2
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    public class NewAttribute : Attribute
+    {
+        public NewAttribute() { }
+        public NewAttribute(string DescriptionParam)
+        {
+            Description = DescriptionParam;
+        }
+
+        public string Description { get; set; }
+    }
+}
